54b1aed272afdc7356a04b5007601120c09171af
luizgustavoou <luizgustavooumbelino@gmail.com>
2024-05-27 10:51:06 -0300

    feature: adicionar import de arquivo js nas outras telas que tem form

---
+++ Treinamento/Scripts/scripts.js
@@ -0,0 +1,26 @@
+﻿function executarAcao(acao) {
+    document.form1.acao.value = acao;
+    document.form1.submit();
+}
+
+
+function estadoInicial() {
+    var elements = document.querySelectorAll('input, select');
+
+    if (elements) {
+        elements.forEach(function (item) {
+            var type = item.type;
+            var tag = item.tagName.toLowerCase();
+
+
+            if (type == 'text' || type == 'hidden' || type == 'password' || tag == 'textarea') {
+                item.value = '';
+            } else if (type == 'checkbox' || type == 'radio') {
+                item.checked = false;
+            } else if (tag == 'select') {
+                item.selectedIndex = -1;
+
+            }
+        })
+    }
+}
