65b5f2774347d4348bfe087a78ca8b5aefbcfb91
luizgustavoou <luizgustavooumbelino@gmail.com>
2024-06-05 12:25:17 -0300

    feature: criar funcao FormatarData e mostrar data inicio e fim no Jogo.cshtml

--- Treinamento/App_Code/Util.cs
+++ Treinamento/App_Code/Util.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Web.WebPages.Html;
+using static Util;
 
 public static class Util
 {
-    public static string ChecaNulo(string str)
+
+    public  enum TypeFormat
+    {
+        BRAZIL_DATE = 23,
+        AMERICAN_DATE = 103,
+        TIME_DEFAULT = 108
+    }
+
+    private static Dictionary<TypeFormat, string> styles = new Dictionary<TypeFormat, string>()
+    {
+        { TypeFormat.BRAZIL_DATE, "dd/MM/yyyy" },
+        { TypeFormat.AMERICAN_DATE, "yy/MM/dd" },
+        { TypeFormat.TIME_DEFAULT, "HH:mm:ss" }
+    };
+
+
+
+public static string ChecaNulo(string str)
     {
         if (string.IsNullOrEmpty(str))
         {
@@ -34,6 +54,14 @@ public static class Util
             model.AddError("alert-danger", ex.Message);
 
         }
+    }
+
+    public static string FormatarData(string data, TypeFormat style)
+    {
+
+        return DateTime.Parse(data).ToString(styles[style]);
+
+        
     }
 }
 
